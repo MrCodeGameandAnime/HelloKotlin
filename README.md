4849c896e9db39a1d5f34bea12a9df920facd28f
MrCodeGameandAnime <mrcodegameandanime@gmail.com>
2020-08-14 07:36:53 -0400

    hello kotlin complete

---
+++ README.md
@@ -0,0 +1,5 @@
+Learning Kotlin from the book "Learning Kotlin for Android Development" by Peter Spath
+
+Project name:
+Hello Kotlin
+
--- hello_kotlin/app/src/main/java/com/example/hellokotlin/Invoice.kt
+++ hello_kotlin/app/src/main/java/com/example/hellokotlin/Invoice.kt
@@ -1,7 +1,6 @@
 package com.example.hellokotlin
 
 import android.util.Log
-import java.util.*
 
 const val TAG:String = "INV"
 class Invoice(val buyersFirstName:String, val buyersLastName: String, val date: String, val goodName: String,
@@ -12,22 +11,52 @@ val amount: Int, val pricePerItem:Double)
     init {
         buyersFullName = buyersFirstName +" "+ buyersLastName
         totalPrice = amount * pricePerItem
+
+    }
+
+    fun getState(): String {
+        return "First name: ${buyersFirstName}\n"+
+                "Last name: ${buyersLastName}\n+"+
+                "Full name: ${buyersFullName}\n"+
+                "Date: ${date}\n"+
+                "Good: ${goodName}\n"+
+                "Amount: ${amount}\n"+
+                "Price per item: ${pricePerItem}\n"+
+                "Total price: ${totalPrice.toString()}"
+    }
+
+    fun tax(taxRate:Double):Double {
+        return taxRate * amount * pricePerItem
+    }
+
+    fun goodInfo():String {
+        return "${amount.toString()} pieces of ${goodName}"
     }
 }
 
 class NextInvoice(val buyersFirstName:String, val buyersLastName: String, val date: String, val goodName: String,
               val amount: Int, val pricePerItem:Double)
 {
+    // Joined declaration and assignment
     val buyersFullName:String = buyersFirstName +" "+ buyersLastName
     val totalPrice:Double = amount * pricePerItem
 }
 
 fun main(args:Array<String>) {
+    // Instantiation
     val firstInvoice = Invoice("Code","Lord","08/12/20","Wes",
         1000000,0.002)
     val secondInvoice = NextInvoice("Wizard","Topaz","08/12/20","Babe",
     1000000000,500.55)
-    Log.d(TAG, "main: ")
+
+    // Usage
+    val state:String = firstInvoice.getState()
+    val tax:Double = firstInvoice.tax(0.07)
+    val info:String = firstInvoice.goodInfo()
+
+
+    Log.d(TAG, "main: " +firstInvoice.buyersFullName)
+    Log.d(TAG, "main: " +secondInvoice.buyersFullName+ " paid " +secondInvoice.totalPrice)
 }
 
 
